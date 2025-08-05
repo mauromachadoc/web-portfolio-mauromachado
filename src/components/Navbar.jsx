fcf3d2811d6f865e6aabd195e2b686a08dabc591
MauroMachado <mauro.machadoc@correo.ucu.edu.uy>
2025-08-05 17:06:09 -0300

    added star background, theme and started navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import { cn } from "@/lib/utils";
+import { useState, useEffect } from 'react';
+
+const navItems = [
+    {name: "Home", href: "#hero"},
+    {name: "About", href: "#about"},
+    {name: "Skills", href: "#skills"},
+    {name: "Projects", href: "#projects"},
+    {name: "Contact", href: "#contact"},
+]
+
+export const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 10);
+        }
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
+
+    return (
+    
+    <nav className={cn("fixed w-full z-40 transition-all duration-300",
+                            isScrolled ? "py-3 bg-background-80 backdrop-blur-md shadow-xs" : "py-5"
+    )}>
+
+        <div className="container flex items-center justify-between">
+            <a
+          className="text-xl font-bold text-primary flex items-center"
+          href="#hero"
+        >
+          <span className="relative z-10">
+            <span className="text-glow text-foreground"> Mauro Machado </span>{" "}
+            Portfolio
+          </span>
+        </a>
+
+        </div>
+    </nav>
+    )
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,15 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
-    return <div className="min-h-screen "> 
-
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden"> 
         {/*Theme Toggle*/}
-
-
-
+        <ThemeToggle/>
         {/*Background Effects*/}
-
-
-
-
+        <StarBackground />
         {/*Navbar */}
-
+        <Navbar/>
 
 
         {/*Main Content*/}
